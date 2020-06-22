163cc5f39510aa60e8dc55ca84c7cea99a077e68
frances-hansen <francesjuniperhansen@users.noreply.github.com>
2020-06-22 15:53:34 -0700

    unit test

---
+++ test/Token.test.js
@@ -0,0 +1,18 @@
+const Token = artifacts.require('./Token')
+
+require('chai')
+.use(require('chai-as-promised'))
+.should()
+
+contract('Token', (accounts) => {
+    describe('deployment', () => {
+        it('tracks the name', async () => {
+            // Fetch token from blockchain
+            const token = await Token.new(1595389544, 1595476244)
+            // Read Token Name
+            const name = await token.name()
+            // Check if token name is 'TOKEN'
+            name.should.equal("TOKEN")
+        })
+    })
+})
